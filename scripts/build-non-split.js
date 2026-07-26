9f7ae07501486397ffb01ce20ff3b578d0f15853
basementuniverse <gordonlarrigan@gmail.com>
2026-07-26 02:05:07 +0100

    Fix build script to work in Node 24

--- scripts/build-non-split.js
+++ scripts/build-non-split.js
@@ -4,6 +4,13 @@
 // See https://github.com/facebook/create-react-app/issues/5306#issuecomment-433425838
 
 const rewire = require("rewire");
+
+// Webpack 4 requests an MD4 hash algorithm that is disabled in modern OpenSSL.
+// Remap it to a supported algorithm so builds work on newer Node versions.
+const crypto = require("crypto");
+const createHash = crypto.createHash;
+crypto.createHash = (algorithm, options) => createHash(algorithm === "md4" ? "sha256" : algorithm, options);
+
 const defaults = rewire("react-scripts/scripts/build.js");
 let config = defaults.__get__("config");
 
